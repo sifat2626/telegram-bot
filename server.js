c90fbe489d6086f88b8f46be89ca4bb6a8be30c7
sifat2626 <ashrafulsifat26@gmail.com>
2024-01-29 16:13:16 +0600

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const userMessageCounts = {};
 const maxMessagesPerMinute = 3;
 
 // Set the URL to the Vercel deployment URL
-const webhookUrl = "https://telegram-bot-five-brown.vercel.app/api/webhook";
+const webhookUrl = "telegram-be98ksa4s-sifat2626.vercel.app/api/webhook";
+bot.getWebHookInfo().then(console.log);
 
 // Set the webhook
 bot.setWebHook(webhookUrl);
